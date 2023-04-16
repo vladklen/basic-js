b580c137d0123bdb3868f3080026c3f6ecfad6c7
vladklen <Vlad6097944@gmail.com>
2023-04-17 02:10:00 +0300

    feat: vigenere test

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(num) {
+  return separate(num);
+
+  function separate(number) {
+    if (number < 10) {
+      return number;
+    }
+
+    let result = number
+      .toString()
+      .split("")
+      .reduce(function (sum, el) {
+        console.log(sum);
+        console.log(el);
+        return +sum + +el;
+      });
+
+    return separate(result);
+  }
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,127 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(boolean) {
+    this.direct = boolean === false ? false : true;
+    this.text = null;
+    this.square = [];
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.symbols = {};
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  generateSquare() {
+    for (let i = 0; i < this.alphabet.length; i++) {
+      let row = this.alphabet.slice(i);
+      row += this.alphabet.slice(0, i);
+      this.square.push(row);
+    }
+  }
+  getSquare() {
+    return this.square;
+  }
+
+  getSymbols(string) {
+    const alph = this.alphabet.split("");
+    const arrayStr = string.toUpperCase().split("");
+    arrayStr.forEach((element, index) => {
+      if (!alph.includes(element)) {
+        this.symbols[index] = element;
+      }
+    });
+  }
+
+  repeatString(firstString, secondString) {
+    let resultString = "";
+    let firstStringLength = firstString.length;
+    let it = 0;
+    for (let i = 0; i < secondString.length; i++) {
+      if (i % firstStringLength === 0) {
+        it = 0;
+      }
+      resultString += firstString[it];
+      it++;
+    }
+    return resultString.toLowerCase();
+  }
+
+  encrypt(string, key) {
+    if (!string || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    this.getSymbols(string);
+    let message = string
+      .replace(/[^a-z ]/gi, "")
+      .replace(/\s/g, "")
+      .toLowerCase();
+    let encryptMessage = "";
+    let newKey = this.repeatString(key, message);
+    this.generateSquare();
+    for (let it = 0; it < message.length; it++) {
+      let i = this.alphabet.toLowerCase().indexOf(message[it]);
+      let j = this.alphabet.toLowerCase().indexOf(newKey[it]);
+      encryptMessage += this.square[i][j];
+    }
+
+    let result = encryptMessage.split("");
+    for (let key in this.symbols) {
+      result.splice(key, 0, this.symbols[key]);
+    }
+    if (this.direct) {
+      return result.join("");
+    } else {
+      return result.reverse().join("");
+    }
+  }
+
+  decrypt(string, key) {
+    if (!string || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    this.getSymbols(string);
+    let message = string
+      .replace(/[^a-z ]/gi, "")
+      .replace(/\s/g, "")
+      .toLowerCase();
+    let decryptMessage = "";
+    let newKey = this.repeatString(key, message);
+    this.generateSquare();
+    for (let it = 0; it < message.length; it++) {
+      let i = this.alphabet.toLowerCase().indexOf(newKey[it]);
+      let j = this.square[i].toLowerCase().indexOf(message[it]);
+      decryptMessage += this.alphabet[j];
+    }
+    let result = decryptMessage.split("");
+    for (let key in this.symbols) {
+      result.splice(key, 0, this.symbols[key]);
+    }
+
+    if (this.direct) {
+      return result.join("");
+    } else {
+      return result.reverse().join("");
+    }
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
